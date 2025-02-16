4d1a805359080d3673d7b17d68dceb087d1517ad
Ayush-kumar-bajwan <ayushbajwan@gmail.com>
2025-02-16 19:47:47 +0530

    url updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["https://womania-health.web.app/"], 
+  origin: ["https://womania-health.web.app"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
