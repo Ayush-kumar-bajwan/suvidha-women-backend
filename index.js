ad9d5372e74c33299e9e36afe0efdf8ea5752047
Ayush-kumar-bajwan <ayushbajwan@gmail.com>
2025-02-16 19:40:07 +0530

    updated url for frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import volunteerRoutes from "./routes/volunteerRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import emailRoutes from './routes/emailRoutes.js';
 import eventRoutes from './routes/eventRoutes.js';
+import path from 'path';
 
 
 dotenv.config();
@@ -14,9 +15,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://womania-health.web.app/"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 // static files
-app.use('/images', express.static('public/images'));
+const __dirname = path.resolve();
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Connect Database
 
